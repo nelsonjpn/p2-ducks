bbfbd36bd251a94a648870b14c65cc9358bc9fec
Cody <hotcococody@gmail.com>
2021-03-09 17:35:07 -0800

    Fixed gif page

--- main.py
+++ main.py
@@ -153,17 +153,12 @@ def rgb_route():
 # connects /flask path of server to render gif.html
 @app.route('/gif/', methods=["GET", "POST"])
 def gif_route():
-    giftype="charm"
-    if request.form.get("charm") is not None:
-        charm=request.form.get("charm")
-    if request.form.get("bulb") is not None:
-        bulb=request.form.get("bulb")
-    if request.form.get("pika") is not None:
-        pika=request.form.get("pika")
-    if request.form.get("squirt") is not None:
-        squirt=request.form.get("squirt")
+    gif=""
+    if request.form.get("selGif") is not None:
+        print("Selected: " + request.form.get("selGif"))
+        gif=request.form.get("selGif")
     update_stats("giftype")
-    return render_template("gif.html", ch=charm, pi=pika, sq=squirt, bu=bulb, projects=data.setup())
+    return render_template("gif.html", gif=gif, projects=data.setup())
 
 @app.route("/project/runtime")
 def runtime_route():
